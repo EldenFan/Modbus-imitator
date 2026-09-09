d59d65f72cf568094c9d5864b8c6a1997df022d9
GigaChmonya <gigachmonya@gmail.com>
2026-09-09 19:18:54 +0300

    fix .gitgnore problem

---
+++ Base/Interafaces/Log/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Interafaces.Log
+{
+    public interface ILogger
+    {
+        event Action<string> LogMessage;
+    }
+}
